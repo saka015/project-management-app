904084fca32ba2050e879f3df954ba21c32fb5b5
Kamal Nayan <sknasylum8454@gmail.com>
2024-09-22 23:56:32 +0530

    [feature]:built basic model

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import SideBar from './components/SideBar'
+import NewProject from './components/NewProject'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='h-screen my-8 flex gap-8'>
+      <SideBar/>
+      <NewProject/>
+    </main>
+  )
 }
 
-export default App;
+export default App
